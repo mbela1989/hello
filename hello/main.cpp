86583971f8e75741640ea6f6056826701b841fe7
mbela1989 <bela1989>
2013-08-25 17:55:15 +0200

    faktorialis
    a faktorialisanak kiszamitasa rekurziv fuggveny segitsegevel

--- hello/main.cpp
+++ hello/main.cpp
@@ -2,8 +2,18 @@
 
 using namespace std;
 
+int fakt (int a) {
+    if ( a<=2)
+        return a;
+    else
+        return a * fakt(a-1);
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a = 0;
+    cout << "a = ";
+    cin >> a;
+    cout << "a! = "<< fakt(a)<< endl;
     return 0;
 }
